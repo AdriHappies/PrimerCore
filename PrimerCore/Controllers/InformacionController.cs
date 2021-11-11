6e861e21f685bbca67da531b681ed64294f68aa7
AdriHappies <adrifegar97@gmail.com>
2021-11-11 17:05:08 +0100

    commit

---
+++ PrimerCore/Controllers/InformacionController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        //usamos la lista index como un menu
+        public IActionResult Index()
+        {
+            //aqui enviamos la info, antes del return
+            return View();
+        }
+
+        public IActionResult InformacionControllerView()
+        {
+            ViewBag.Nombre = "Alumno";
+            ViewData["Descripcion"] = "Estoy aprendiendo Net Core";
+            ViewBag.Year = 2021;
+            return View();
+        }
+        public IActionResult InformacionControllerViewModel()
+        {
+            List<Persona> listapersonas = new List<Persona>();
+            for (int i = 1; i <= 10; i++)
+            {
+                Persona per = new Persona();
+                per.Nombre = "Persona " + i;
+                per.Edad = 20 + i;
+                per.Email = "persona" + i + "@correo.com";
+                listapersonas.Add(per);
+            }
+            //enviamos el modelo a la vista
+            return View(listapersonas);
+        }
+    }
+}
